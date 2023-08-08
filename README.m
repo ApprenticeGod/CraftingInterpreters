5e549402223f0027958aa4539d1caa36d865fe29
STM-NEW <76831525+STM-NEW@users.noreply.github.com>
2023-08-08 12:36:13 +0800

    Create README.m

---
+++ README.m
@@ -0,0 +1,5 @@
+这是个从别人那里获得的《Crafting Interpreters》的中文翻译版。
+
+英文版原网站：http://craftinginterpreters.com/contents.html
+
+中文版来源：https://gitee.com/xuejianxinokok/mysqldoc/blob/master/craftinginterpreters/0_00.contents/contents.md
